b709eca8895ce12eb17a5f32641dbd08db39a105
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-10-07 08:17:22 +0545

    form

--- Frontend/src/pages/auth/components/form/Form.jsx
+++ Frontend/src/pages/auth/components/form/Form.jsx
@@ -1,6 +1,26 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
 
 const Form = ({type}) => {
+  const [data,setData] = useState({
+    email : '',
+    username : '',
+    password : ''
+
+  })
+  const handleChange=(e)=>{
+    const {name,value} = e.target
+    setData({
+      ...data,
+      [name] : value
+    })
+
+  }
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+
+  }
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
     <div className="relative py-3 sm:max-w-xl sm:mx-auto">
@@ -13,22 +33,23 @@ const Form = ({type}) => {
           <div>
             <h1 className="text-2xl font-semibold">{type}</h1>
           </div>
+          <form onSubmit = {handleSubmit} >
           <div className="divide-y divide-gray-200">
             <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
               <div className="relative">
-                <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+                <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange ={handleChange} required />
                 <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
               </div>
               {
                 type === 'Register' && (
                     <div className="relative">
-                <input autoComplete="off" id="username" name="username" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+                <input autoComplete="off" id="username" name="username" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange ={handleChange} required />
                 <label htmlFor="userName" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
               </div>
                 )  
               }
               <div className="relative">
-                <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" onChange ={handleChange} required />
                 <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
               </div>
               <div className="relative">
@@ -36,6 +57,7 @@ const Form = ({type}) => {
               </div>
             </div>
           </div>
+          </form>
         </div>
   
         <div className="w-full flex justify-center">
