9ee369a025e4bffd4c628167e199e8a2f4e63175
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-10-18 11:45:25 +0545

    config naming

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -2,7 +2,7 @@
 import { useNavigate } from "react-router-dom"
 import Form from "./components/form/Form"
 import axios from "axios"
-import { baseurl } from "../../../CONFIG.JS"
+import { baseurl } from "../../../config.js"
 
 
 const Login = () => {
--- Frontend/src/pages/auth/Register.jsx
+++ Frontend/src/pages/auth/Register.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import Form from './components/form/Form'
 import axios from 'axios'
-import { baseurl } from '../../../CONFIG.JS'
+import { baseurl } from '../../../config.js'
 
 const Register = () => {
   const navigate = useNavigate()
